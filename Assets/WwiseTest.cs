3bb336013eb7d70d5aec1f1e2e84674590594046
Cuttlefish <zh4n@live.co.uk>
2022-07-21 20:26:55 +0100

    Wwise working
    - just need to set up sound banks!

---
+++ Assets/WwiseTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseTest : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AkSoundEngine.PostEvent("soundTest", gameObject);//  Wwise test!
+            print("sound should be played!!");
+        }
+    }
+}
